cb106de9ddfea01bf2004c6197466675d7a54c9f
Jean-François Reboud <jean-francois.reboud@owkin.com>
2022-10-19 21:31:12 +0200

    CI ?

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// The swift-tools-version declares the minimum version of Swift required
+// to build this package.
 
 import PackageDescription
 
@@ -9,7 +10,6 @@ let package = Package(
         .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MAKit",
             targets: ["MAKit", "MATestsUtils"]
@@ -22,8 +22,6 @@ let package = Package(
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MAKit",
             dependencies: [],

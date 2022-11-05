0f028ca50104bb14b674d86c1e0b32335d65d27a
Jean-François Reboud <jean-francois.reboud@owkin.com>
2022-11-05 23:44:09 +0100

    Test conv [skip ci]

--- Sources/MAKit/Layer2D/Convolution2D.swift
+++ Sources/MAKit/Layer2D/Convolution2D.swift
@@ -1293,7 +1293,7 @@ public class Convolution2D: BN2D
             
             let w = command.threadExecutionWidth
             let h = command.maxThreadsPerThreadgroup / w
-            let threadsPerThreadgroup = MTLSizeMake(w, h, 1)
+            let threadsPerThreadgroup = MTLSizeMake(8, 8, 1)
             let threadsPerGrid = MTLSize(
                 width: nbChannels * width,
                 height: batchSize * height,
@@ -1483,7 +1483,7 @@ public class Convolution2D: BN2D
             
             let w = command.threadExecutionWidth
             let h = command.maxThreadsPerThreadgroup / w
-            let threadsPerThreadgroup = MTLSizeMake(w, h, 1)
+            let threadsPerThreadgroup = MTLSizeMake(8, 8, 1)
             let threadsPerGrid = MTLSize(
                 width: nbChannelsPrev * layerPrev.width,
                 height: batchSize * layerPrev.height,

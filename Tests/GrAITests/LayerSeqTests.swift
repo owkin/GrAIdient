76aab27bea22e338bdbae76c290af247f8af7ab8
Jean-François Reboud <jean-francois.reboud@owkin.com>
2023-02-27 10:18:20 +0100

    LayerSeqFlowTests (Ko)

--- Tests/GrAITests/LayerSeqTests.swift
+++ Tests/GrAITests/LayerSeqTests.swift
@@ -92,3 +92,73 @@ class LayerSeqGradTests: Input2DMSE1DCase
         run(trainer)
     }
 }
+
+// -----------------------------------------------------------------------------
+// Compare GPU gradients with CPU ones through time.
+// We expect to see errors ~ 1e-7 and less.
+// -----------------------------------------------------------------------------
+class LayerSeqFlowTests: Input2DMSE1DCase
+{
+    private func _buildTrainer(_ model: String) -> FlowTrainer
+    {
+        let trainer = FlowTrainer(
+            name: "LayerSeq",
+            params: optimizerParams
+        )
+        trainer.build()
+        {
+            (context: ModelContext) in
+            buildModel(model: model, context: context)
+        }
+        return trainer
+    }
+    
+    func buildModel(model: String, context: ModelContext)
+    {
+        let params = GrAI.Model.Params(context: context)
+        
+        var layer: Layer2D = Input2D(
+            nbChannels: 1, width: width, height: height, params: params
+        )
+        
+        layer = Convolution2D(
+            layerPrev: layer, size: 1, nbChannels: 3, stride: 1,
+            activation: LeakyReLU.str, biases: true, bn: false, params: params
+        )
+        
+        var layerSeq: LayerSeq
+        switch model
+        {
+        case "FullyConnectedPatch":
+            layerSeq = FullyConnectedPatch(
+                layerPrev: layer, patch: 2, nbNeurons: 5,
+                activation: LeakyReLU.str, biases: true, params: params
+            )
+            
+        default:
+            fatalError("Unreachable.")
+        }
+        
+        var head: Layer1D = AvgPoolSeq(layerPrev: layerSeq, params: params)
+        
+        head = FullyConnected(
+            layerPrev: head, nbNeurons: 1,
+            activation: LeakyReLU.str, biases: true, params: params
+        )
+        
+        _ = MSE1D(layerPrev: head, params: params)
+    }
+    
+    func testFullyConnectedPatch() throws
+    {
+        let trainer = _buildTrainer("FullyConnectedPatch")
+        run(trainer)
+    }
+    
+    func testFullyConnectedPatchSample() throws
+    {
+        GrAI.Gradient.sample = true
+        let trainer = _buildTrainer("FullyConnectedPatch")
+        run(trainer)
+    }
+}
